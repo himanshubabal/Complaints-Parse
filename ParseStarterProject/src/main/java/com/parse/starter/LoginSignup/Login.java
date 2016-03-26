5e13e923c39a5fb0cd53b63736adbfe7938fd70d
babal_himanshu <babal.himanshu@gmail.com>
2016-03-26 21:55:41 +0530

    Navigation bar working

--- ParseStarterProject/src/main/java/com/parse/starter/LoginSignup/Login.java
+++ ParseStarterProject/src/main/java/com/parse/starter/LoginSignup/Login.java
@@ -1,9 +1,11 @@
 package com.parse.starter.LoginSignup;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -12,17 +14,23 @@ import android.widget.EditText;
 import com.parse.LogInCallback;
 import com.parse.ParseException;
 import com.parse.ParseUser;
+import com.parse.starter.MainActivity;
 import com.parse.starter.R;
 
 public class Login extends AppCompatActivity{
     EditText userName;
     EditText password;
     Button logIn;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
+
+        //TODO ->
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         userName = (EditText) findViewById(R.id.userName_EditText);
         password = (EditText) findViewById(R.id.password_editText);
@@ -37,6 +45,8 @@ public class Login extends AppCompatActivity{
             public void done(ParseUser user, ParseException e) {
                 if (e == null){
                     Log.i("parse-LogIn", "Log In successful");
+                    Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(i);
                 }
                 else {
                     Log.i("parse-LogIn", e.toString());
